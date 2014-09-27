ca60fc810e19a8726308a713c99894c6e6134931
Ernest Han <ernest.hec@gmail.com>
2014-09-27 19:11:43 +0800

    Log current workspace status

--- js/workspace.js
+++ js/workspace.js
@@ -1,3 +1,5 @@
+var workspace = [];
+
 $(document).ready(function(){
     $('.command-object').draggable({
         snap:true,
@@ -11,8 +13,19 @@ $(document).ready(function(){
         drop: function(event, ui){
             $(this).find('h4').remove();
             $(ui.draggable).clone().appendTo(this);
-            console.log($("#workspace-main .workspace-row").last());
             $("#workspace-main .workspace-row").first().clone().appendTo($(this).parent().parent()).removeClass("hidden");
+            var data = $(event.target).find('.command-object').data();
+            //var lineData = $(event.target);
+            //console.log(lineData);
+            workspace[workspace.length]={
+                type: data.type,
+                direction: data.direction,
+                steps: data.steps
+            };
+
+            console.log(workspace);
+
         }
     });
+
 });
