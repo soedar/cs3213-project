a138ed86daa4d2486d24a5de030ff689d7478313
Ernest Han <ernest.hec@gmail.com>
2014-09-28 00:33:37 +0800

    Refactor abit

--- js/workspace.js
+++ js/workspace.js
@@ -1,6 +1,7 @@
 var workspace = [];
 
 $(document).ready(function(){
+    //From command palette, we just need to clone
     $('.command-object').draggable({
         snap:true,
         snapMode: "inner",
@@ -8,24 +9,41 @@ $(document).ready(function(){
         helper: "clone"
     });
 
+    //Start listening to drops
+    listenDrag();
+
+});
+
+var listenDrag = function(){
+
     $('.command-placeholder').droppable({
         accept: ".command-object",
         drop: function(event, ui){
+
+            //Remove the existing text
             $(this).find('h4').remove();
+
+            //Create a new command-object
             $(ui.draggable).clone().appendTo(this);
+
+            //Create a new placeholder
             $("#workspace-main .workspace-row").first().clone().appendTo($(this).parent().parent()).removeClass("hidden");
+
+            //Save data
             var data = $(event.target).find('.command-object').data();
-            //var lineData = $(event.target);
+            var rowData = $(this).data();
+            console.log(rowData);
             //console.log(lineData);
             workspace[workspace.length]={
+                line: rowData.line,
+                nestLevel: rowData.nestLevel,
                 type: data.type,
                 direction: data.direction,
                 steps: data.steps
             };
 
-            console.log(workspace);
+            console.log("Current workspace state: " + JSON.stringify(workspace));
 
         }
     });
-
-});
+}
