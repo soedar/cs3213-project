f9c4f321b38b9bd80d86f6fa3a974e6e46f7c44f
sudarsangp <sudarsangp@gmail.com>
2014-10-15 08:09:21 +0800

    implemented delete for command drag and dropped and also implemented clear all functionality

--- workspace/js/workspace.js
+++ workspace/js/workspace.js
@@ -2,24 +2,29 @@
 
     var app = angular.module('visualide', ['ngDragDrop']);
     
-    app.controller('DragCommandController', ['$scope',function($scope, $timeout){
+    app.controller('DragCommandController', ['$scope',function($scope){
         // $scope.list1 = {title: 'AngularJS - Drag Me'};
         // $scope.list2 = {};
 
-       
-       // console.log($scope.stepsValue);
-        $scope.stepsValue = 0;
         $scope.navigation = [{'command':'Move', 'direction':'Up','steps':0},{'command':'Move', 'direction':'Down','steps':0},{'command':'Move', 'direction':'Left','steps':0},{'command':'Move', 'direction':'Right','steps':0}]
         $scope.commandsWorkspace = [];
-     	$scope.hideMe = function() {
+     	
+        $scope.hideMe = function() {
 		    return $scope.commandsWorkspace.length > 0;
 		  }
-		$scope.sendStepsValue = function(){
-			//console.log('in function' + $scope.stepsValue);
-		}
+		
 		$scope.sendData = function(){
 			console.log($scope.commandsWorkspace);
-		}
+		};
+        
+        $scope.removeCommands = function(index) {
+          $scope.commandsWorkspace.splice(index, 1);
+        };
+
+        $scope.clearCommands = function() {
+            $scope.commandsWorkspace = [];
+        };
+        
     }]);
 
 })();
