0e7e1936d0c6f8c97dcffb5e3ed33f64641c13e6
soedar <soedar.sg@gmail.com>
2014-10-18 17:53:26 +0800

    Integrated game engine on the backend

--- scratch/common/models/command.js
+++ scratch/common/models/command.js
@@ -1,9 +1,12 @@
-module.exports = function(Command) {
+var Engine = require('../../engine/engine.js');
 
+module.exports = function(Command) {
+  console.log(Engine);
   Command.afterRemote('create', function(ctx, unused, next) {
     var VisualEvent = Command.app.models.VisualEvent;
 
-    VisualEvent.create({commandId: ctx.result.id, events: {list: [1,2]}}, function(error, e) {
+    var engine = new Engine(ctx.result.map, ctx.result.commands);
+    VisualEvent.create({commandId: ctx.result.id, events: engine.run()}, function(error, e) {
       ctx.res.send(ctx.result);
     });
   });
---
+++ scratch/engine/engine.js
@@ -0,0 +1,108 @@
+'use strict';
+var _ = require('lodash');
+
+function Engine(map, playerActions) {
+  this.map = map;
+  this.playerActions = playerActions;
+}
+
+Engine.prototype.run = function() {
+  var objectsMemory = _.cloneDeep(this.map.objects);
+
+  var gameOutput = {};
+  gameOutput.mapSize = this.map.mapSize;
+  gameOutput.objects = _.cloneDeep(this.map.objects);
+  gameOutput.events = [];
+
+
+  var players = objectsMemory.filter(function(object) {
+    return object.type === 'player';
+  });
+
+  // Assume we only have one player for now
+  var player = players[0];
+  this.playerActions.forEach(function(action) {
+    var e = this.makeEvent(action, player, objectsMemory);
+    if (e) {
+      gameOutput.events.push(e);
+    }
+  }.bind(this));
+
+  return gameOutput;
+};
+
+Engine.prototype.makeEvent = function(action, player, objectsMemory) {
+  if (action.command === 'Move') {
+    var new_xy = _.clone(player.xy);
+    switch(action.commandType) {
+      case 'Left':
+        new_xy.x -= action.parameters;
+        break;
+      case 'Right':
+        new_xy.x += action.parameters;
+        break;
+      case 'Up':
+        new_xy.y -= action.parameters;
+        break;
+      case 'Down':
+        new_xy.y += action.parameters;
+        break;
+    }
+
+    // Ignore moves that goes out of the boundary
+    if (new_xy.x < 0 || new_xy.y < 0 ||
+        new_xy.x >= this.map.mapSize || new_xy.y >= this.map.mapSize) {
+          return null;
+        }
+
+    // We have determine a valid move, so we reduce the health
+    player.model.health -= 1;
+
+    // See if there are any objects that has the coordinate
+    var obj = objectsMemory.filter(function(object) {
+      return new_xy.x === object.xy.x && new_xy.y === object.xy.y;
+    });
+
+    var e = {
+      id: player.id,
+      update: {}
+    };
+
+    // Player will definitely reduce health at every turn
+    e.update[player.id] = {health: player.model.health};
+
+    // There is an object at the same location as the new location
+    if (obj.length > 0) {
+      // Assume that only one object can be at a position
+      if (obj[0].type === "coin") {
+        player.xy = new_xy;
+        player.model.coin++;
+
+        e.type = 'pickCoin';
+        e.target = obj[0].id;
+        e.update[player.id].coin = player.model.coin;
+      }
+      else if (obj[0].type === "spinach") {
+        player.xy = new_xy;
+        player.model.health += 5;
+
+        e.type = 'pickSpinach';
+        e.target = obj[0].id;
+        e.update[player.id].health = player.model.health;
+      }
+    }
+
+    // Nothing there, so we can move to the new location
+    else {
+      player.xy = new_xy;
+      e.type = 'move';
+    }
+
+
+    // Update the location of the player
+    e.xy = _.clone(player.xy);
+    return e;
+  }
+};
+
+module.exports = Engine;
