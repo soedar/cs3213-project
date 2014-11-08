27fd59b3a33dd6a3f9b1687e86b223f670becec7
SeanNguyen <firestormkjc.1993@gmail.com>
2014-11-08 22:14:41 +0800

    solving syntax warning in engine.js

--- frontend/app/scripts/engine/engine.js
+++ frontend/app/scripts/engine/engine.js
@@ -9,7 +9,7 @@ function Engine(map, playerActions) {
 //main methods
 
 Engine.prototype.run = function() {
-  console.log("START ENGINE");
+  console.log('START ENGINE'); 
   var gameOutput = {};
   gameOutput.mapSize = this.map.mapSize;
   gameOutput.objects = _.cloneDeep(this.map.objects);
@@ -45,21 +45,17 @@ Engine.prototype.makeEvents = function(actions) {
 		    if (e) { events.push(e); }
 		}
 	}
-	return events
+	return events;
 };
 
 Engine.prototype.makeEvent = function(action) {
   if (action.command === 'Move') {
 	var player = this.getPlayer();
-	var direction;
-	if (action.commandType === 'Direction') {
-		direction = this.variables['direction'];
-	} else {
-		direction = action.commandType;
-	}
+	var direction = action.commandType;
 	
-	if (!this.isValidMove(direction))
+	if (!this.isValidMove(direction)) {
 		return null;
+	}
 	
 	this.updatePlayerPosition(direction);
 	var obj = this.popObjectAtPosition(player.xy); //get and delete an object at a specific location
@@ -75,13 +71,13 @@ Engine.prototype.makeEvent = function(action) {
     // There is an object at the same location as the new location
     if (obj) {
       // Assume that only one object can be at a position
-      if (obj.type === "coin") {
+      if (obj.type === 'coin') {
         player.model.coin++;
         e.type = 'pickCoin';
         e.target = obj.id;
         e.update[player.id].coin = player.model.coin;
       }
-      else if (obj.type === "spinach") {
+      else if (obj.type === 'spinach') {
         player.model.health += 5;
         e.type = 'pickSpinach';
         e.target = obj.id;
@@ -97,71 +93,61 @@ Engine.prototype.makeEvent = function(action) {
 //helping methods
 
 Engine.prototype.isConditionSatisfied = function(object, operator, parameter) {
-	if (object === "coin") {
+	if (object === 'coin') {
 		var player = this.getPlayer();
 		switch (operator) {
 			case '==':
-				return player.model.coin == parameter;
-				break;
+				return player.model.coin === parameter;
 			case '!=':
-				return player.model.coin != parameter;
-				break;
+				return player.model.coin !== parameter;
 			case '>':
 				return player.model.coin > parameter;
-				break;
 			case '>=':
 				return player.model.coin >= parameter;
-				break;
 			case '<':
 				return player.model.coin < parameter;
-				break;
 			case '<=':
 				return player.model.coin <= parameter;
-				break;
 		}
-	} else if (object === "spinach") {
+	} else if (object === 'spinach') {
 	
-	} else if (object === "wall") {
+	} else if (object === 'wall') {
 	
 	}
-}
+};
 
 Engine.prototype.popObjectAtPosition = function(xy) {
 	var obj;
 	var i;
 	for (i = 0; i < this.map.objects.length; i++) {
 		var object = this.map.objects[i];
-		if (object.type !== "player" && (xy.x === object.xy.x && xy.y === object.xy.y)) {
+		if (object.type !== 'player' && (xy.x === object.xy.x && xy.y === object.xy.y)) {
 			obj = object;
 			this.map.objects.splice(i, 1);
 			return obj;
 		}
 	}
 	return null;
-}
+};
 
 Engine.prototype.isValidMove = function(direction) {
 	var player = this.getPlayer();
 	switch(direction) {
       case 'Left':
-        if (player.xy.x > 0)
-			return true;
+        if (player.xy.x > 0) { return true; }
 		break;
       case 'Right':
-        if (player.xy.x < this.map.mapSize - 1)
-			return true;
+        if (player.xy.x < this.map.mapSize - 1) { return true; }
 		break;
       case 'Up':
-        if (player.xy.y > 0)
-			return true;
+        if (player.xy.y > 0) { return true; }
 		break;
       case 'Down':
-        if (player.xy.y < this.map.mapSize - 1)
-			return true;
+        if (player.xy.y < this.map.mapSize - 1) { return true; }
 		break;
     }
 	return false;
-}
+};
 
 Engine.prototype.updatePlayerPosition = function(direction) {
 	var player = this.getPlayer();
@@ -179,52 +165,54 @@ Engine.prototype.updatePlayerPosition = function(direction) {
         player.xy.y++;
 		break;
     }
-}
+};
 
 Engine.prototype.getPlayer = function () {
 	var players = this.map.objects.filter(function(object) {
 		return object.type === 'player';
 	  });
 	return players[0];
-}
+};
 
 Engine.prototype.getIfActionList = function(actions, startingPoint) {
 	var ifActions = [];
 	if (actions[startingPoint].command === 'If') {
 		var i;
 		for (i = startingPoint + 1; i < actions.length; i++) {
-			if (actions[i].command === 'End If' && actions[startingPoint].nestLevel == actions[i].nestLevel)
+			if (actions[i].command === 'End If' && actions[startingPoint].nestLevel === actions[i].nestLevel) {
 				break;
+			}
 			ifActions.push(actions[i]);
 		}
 	}
 	return ifActions;
-}
+};
 
 Engine.prototype.getWhileActionList = function(actions, startingPoint) {
 	var whileActions = [];
 	if (actions[startingPoint].command === 'While') {
 		var i;
 		for (i = startingPoint + 1; i < actions.length; i++) {
-			if (actions[i].command === 'End While' && actions[startingPoint].nestLevel == actions[i].nestLevel) {
+			if (actions[i].command === 'End While' && actions[startingPoint].nestLevel === actions[i].nestLevel) {
 				break;
 			}
 			whileActions.push(actions[i]);
 		}
 		return whileActions;
 	}
-}
+};
 
 Engine.prototype.getObjectsInRange = function(checkedObject, range, player) {
 	var results = [];
 	var checkedTargets = this.map.objects.filter(function(value) { return value.type === checkedObject;});
 	checkedTargets.forEach(function(object) {
 		var distance = Math.abs(player.xy.x - object.xy.x) + Math.abs(player.xy.y - object.xy.y);
-		if (distance <= range)
+		if (distance <= range) {
 			results.push(object);
+		}
 	});
 	return results;
-}
+};
 
 Engine.prototype.getDirection = function (checkedObject, range, player) {
 	var objectsInRange = this.getObjectsInRange(checkedObject, range, player);
@@ -235,12 +223,16 @@ Engine.prototype.getDirection = function (checkedObject, range, player) {
 		return 'none';
 	}
 	
-	if (object.xy.x < player.xy.x)
-		return 'Left';
-	if (object.xy.x > player.xy.x)
+	if (object.xy.x < player.xy.x) { 
+		return 'Left'; 
+	}
+	if (object.xy.x > player.xy.x) {
 		return 'Right';
-	if (object.xy.y < player.xy.y)
+	}
+	if (object.xy.y < player.xy.y) {
 		return 'Up';
-	if (object.xy.y > player.xy.y)
+	}
+	if (object.xy.y > player.xy.y) {
 		return 'Down';
-}
+	}
+};
